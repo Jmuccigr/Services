ba6474646c51c7c5356e2b51f6289606885fdff7
John Muccigrosso <john@muccigrosso.org>
2015-10-13 14:18:05 -0400

    Fixed counter.
    Counter was resetting resulting in no second line being printed. Also
    changed loop to make # of items in header dictate the # of columns in
    table, instead of using the item count from line 2.

--- Convert to markdown table/Markdown - convert to table.pl
+++ Convert to markdown table/Markdown - convert to table.pl
@@ -1,15 +1,16 @@
-﻿while (<>) {
+﻿my $counter = 1;
+while (<>) {
 	chomp;
 	@t = split ("\r",$_);
-	$counter = 0;
 
 	foreach $elem (@t) {
-		$counter++;
 		$n = $elem =~ s/\t/\|/g;
 		$elem =~ s/^/\|/;
 		$elem =~ s/$/\|/;
-		if ($counter == 2 ) {printf "|" . "---|"x($n+1) . "\n" };
-		print $elem, "\n";
+		print $elem;
+		if ($counter == 1 ) {printf "\n|" . "---|"x($n+1)};
+		$counter ++;
 	}
+
 	print "\n";
 }
